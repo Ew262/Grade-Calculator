5b831b013e5f5ac10650dddd047f4761fdf798f6
in43out43 <70719395+in43out43@users.noreply.github.com>
2020-09-03 16:24:21 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@
 //
 //  Created by Andrew Cleveland on 9/3/20.
 //  Copyright © 2020 Andrew Cleveland. All rights reserved.
+//	CSC201 D01B Scott Davis
 //
+//	Takes input from user including name and grades of 3 assignments to calculate grade percentage and
+//	gives feedback such as "Excellent!" or "Failed" etc
+
 
 #include <iostream>
 #include <string>
@@ -15,20 +19,22 @@ using namespace std;
 
 int main()
 {
-	string lastName;
-	int stuScore = 0;
-	int totPoints = 0;
-	int gradeCounter;
-	float grade;
-	string message;
+	string lastName;	//user's name
+	int stuScore = 0;	//stores individual score from user input
+	int totPoints = 0;	//stores sum of user scores
+	int gradeCounter;	//counts the number of grades input from the user
+	float grade;		//numerical mean of grades input as a percentage
+	string message;		//message associated with each grade range
 	
-	cout << "Enter last name: ";
-	cin >> lastName;
+	cout << "Enter last name: ";	//prints instruction
+	cin >> lastName;				//takes user's last name
+	
+	//loop to repeatedly ask user for grades, verify input as valid, and upon verification, add each to the total,
 	for (gradeCounter = 0; gradeCounter < 3; gradeCounter++)
 	{
-		cout << "Enter grade " << gradeCounter + 1 << ": ";
-		cin >> stuScore;
-		if((stuScore < 0)  || (stuScore > 100) || (!cin))
+		cout << "Enter grade " << gradeCounter + 1 << ": ";	//	prints instructions for user
+		cin >> stuScore;									//	reads in user input for grade
+		if((stuScore < 0)  || (stuScore > 100) || (!cin))	//	checks input to ensure all input are numerical and in the proper range
 		{
 			cout << "That input was not accepted, please enter a number between 0 and 100" << endl;
 			cin.clear();
@@ -41,7 +47,7 @@ int main()
 		totPoints += stuScore;
 		}
 	}
-	
+//	calculates grade percentage, casts int's into a grade
 	grade = static_cast<float>(totPoints) / gradeCounter;
 	
 	cout << "Average: " << static_cast<int>(grade+0.5) << "%, ";
